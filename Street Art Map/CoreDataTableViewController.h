99b9e54f481073ba1a6c17f5e833afc6bd48b5a2
AlexMS2015 <Alex@192-168-1-5.tpgi.com.au>
2016-01-10 13:56:26 +1100

    Commit

--- Street Art Map/CoreDataTableViewController.h
+++ Street Art Map/CoreDataTableViewController.h
@@ -10,16 +10,4 @@
 // abstract method. this is called when the context is set. you may need to set the context manually.
 -(void)setupFetchedResultsController;
 
-/*
-// Causes the fetchedResultsController to refetch the data.
-// You almost certainly never need to call this.
-// The NSFetchedResultsController class observes the context
-//  (so if the objects in the context change, you do not need to call performFetch
-//   since the NSFetchedResultsController will notice and update the table automatically).
-// This will also automatically be called if you change the fetchedResultsController @property.
-- (void)performFetch;
-
-// Set to YES to get some debugging output in the console.
-@property BOOL debug;*/
-
 @end
